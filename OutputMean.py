0bc0df2703fceac7d49beed92cfc7eabc077aeda
Jerry Wu <a91082900@gmail.com>
2021-09-27 00:37:40 +0800

    display two showstat button in a row for monster, weapon and armor

--- OutputMean.py
+++ OutputMean.py
@@ -160,8 +160,14 @@ class Output:
     @sending
     def stat_monsters(self, identifier, stage):
         kb_list = []
-        for name in Monsters[stage]:
-            kb_list.append([InlineKeyboardButton(text=name, callback_data=f"showstat monster {stage} {name}")])
+        display = list(Monsters[stage])
+        for name1, name2 in zip(display[::2], display[1::2]):
+            kb_list.append([
+                InlineKeyboardButton(text=name1, callback_data=f"showstat monster {stage} {name1}"), 
+                InlineKeyboardButton(text=name2, callback_data=f"showstat monster {stage} {name2}")
+            ])
+        if len(display) % 2:
+            kb_list.append([InlineKeyboardButton(text=display[-1], callback_data=f"showstat monster {stage} {display[-1]}")])
         kb_list.append([InlineKeyboardButton(text="上一層", callback_data="showstat monster")])
         keyboard = InlineKeyboardMarkup(inline_keyboard=kb_list)
         self.bot.editMessageText("請選擇", chat_id=identifier[0], message_id=identifier[1], reply_markup=keyboard)
@@ -199,9 +205,15 @@ class Output:
         weapon_list = list(Weapons.items())
         if page:
             kb_list.append([InlineKeyboardButton(text="<<", callback_data=f"showstat weapon {page-1}")])
-        for i in range(page*6, min(page*6+6, len(weapon_list))):
-            name, param = weapon_list[i]
-            kb_list.append([InlineKeyboardButton(text=name, callback_data="showweapon "+name)])
+
+        display = weapon_list[page*6:min(page*6+6, len(weapon_list))]
+        for item1, item2 in zip(display[::2], display[1::2]):
+            kb_list.append([
+                InlineKeyboardButton(text=item1[0], callback_data=f"showweapon {item1[0]}"),
+                InlineKeyboardButton(text=item2[0], callback_data=f"showweapon {item2[0]}")
+            ])
+        if len(display) % 2:
+            kb_list.append([InlineKeyboardButton(text=display[-1][0], callback_data=f"showweapon {display[-1][0]}")])
         if page*6 + 6 < len(weapon_list):
             kb_list.append([InlineKeyboardButton(text=">>", callback_data=f"showstat weapon {page+1}")])
         kb_list.append([InlineKeyboardButton(text="上一層", callback_data="showstat")])
@@ -214,9 +226,15 @@ class Output:
         armor_list = list(Armors.items())
         if page:
             kb_list.append([InlineKeyboardButton(text="<<", callback_data=f"showstat armor {page-1}")])
-        for i in range(page*6, min(page*6+6, len(armor_list))):
-            name, param = armor_list[i]
-            kb_list.append([InlineKeyboardButton(text=name, callback_data="showarmor "+name)])
+
+        display = armor_list[page*6:min(page*6+6, len(armor_list))]
+        for item1, item2 in zip(display[::2], display[1::2]):
+            kb_list.append([
+                InlineKeyboardButton(text=item1[0], callback_data=f"showarmor {item1[0]}"),
+                InlineKeyboardButton(text=item2[0], callback_data=f"showarmor {item2[0]}")
+            ])
+        if len(display) % 2:
+            kb_list.append([InlineKeyboardButton(text=display[-1][0], callback_data=f"showarmor {display[-1][0]}")])
         if page*6 + 6 < len(armor_list):
             kb_list.append([InlineKeyboardButton(text=">>", callback_data=f"showstat armor {page+1}")])
         kb_list.append([InlineKeyboardButton(text="上一層", callback_data="showstat")])
